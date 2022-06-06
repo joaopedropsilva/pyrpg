3e60e493e96dbe15a7a2fbd7dd6361f70be9f36f
johnny <jp.pianissoli@gmail.com>
2022-06-06 11:54:08 -0300

    feat!: finish create_save_game function

--- src/animations.py
+++ src/animations.py
@@ -5,10 +5,22 @@ import keyboard as kb
 from structures.game_constants import DEFAULT_ANIMATION_SPEED
 
 
-def timed_writing(text, animation_speed=DEFAULT_ANIMATION_SPEED):
+def timed_writing_animation(text, animation_speed=DEFAULT_ANIMATION_SPEED):
     for letter in text:
         stdout.write(letter)
         stdout.flush()
         # if (kb.on_press('right')):
         #     animation_speed *= 2
         sleep(animation_speed)
+
+
+def menu_transfer_animation():
+    pass
+
+
+def creating_new_game_animation():
+    pass
+
+
+def loading_game_animation():
+    pass
