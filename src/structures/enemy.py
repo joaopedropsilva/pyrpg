7702d910204bb3faf2774f31ed51ff6720dbee07
johnny <jp.pianissoli@gmail.com>
2022-06-20 07:01:50 -0300

    feat(enemy): add setter method to Enemy class

--- src/structures/enemy.py
+++ src/structures/enemy.py
@@ -4,3 +4,27 @@ class Enemy:
         self.hp = hp
         self.atk = attack
         self.dfs = defense
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, new_hp):
+        self._hp = new_hp
+
+    @property
+    def atk(self):
+        return self._atk
+
+    @atk.setter
+    def atk(self, new_atk):
+        self._atk = new_atk
+
+    @property
+    def dfs(self):
+        return self._dfs
+
+    @dfs.setter
+    def dfs(self, new_dfs):
+        self._dfs = new_dfs
